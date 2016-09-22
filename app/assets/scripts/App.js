24a9b4c4d812f06ff076a1fa6f213ffa9bfd2bba
i-break-codes <vaibhav@browserstack.com>
2016-09-23 01:36:31 +0530

    temporary push

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,30 @@
 var App = function() {
+  var socket = io();
+  
   function init() {
-    
+    getLogs();
+    addToLog();
+  }
+  
+  function addToLog() {
+    socket.on('exception-logged', function(msg) {
+      var template;
+      template  = '<li>';
+      template += '<div class="log-item-wrapper">';
+      template += '<div class="log-info clear">';
+      template += '<div class="log-badge development-badge">' + msg.data.badge + '</div>';
+      template += '<div class="log-time">' + msg.data.created_at + '</div>';
+      template += '</div>';
+      template += '<h4>' + msg.data.error + '</h4>';
+      template += '</div>';
+      template += '</li>';
+  
+      $('.log-list ul').append(template);
+    });
+  }
+  
+  function getLogs() {
+  
   }
   
   return {
--- server.js
+++ server.js
@@ -1,18 +1,81 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var mysql = require('mysql');
+var bodyParser = require('body-parser')
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'logerr'
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+});
+
 
 app.set('view engine', 'ejs');
-
 app.use(express.static(__dirname + '/app'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+})); 
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   var dateTime = new Date().toLocaleString();
   
   console.log('Serving Index');
   
-  res.render(__dirname + '/app/views/pages/index', {
-    bugsCount: 0 //temp
+  connection.query('SELECT id, err, badge, created_at, read FROM tbl_logs', function(err, rows, fields) {
+    if (err) throw err;
+    res.render(__dirname + '/app/views/pages/index', {records: rows});
   });
 });
 
-app.listen(8080);
+app.post('/log', function(req, res) {
+  var params = req.body;
+  
+  var d = new Date(params.datetime),
+    dformat = [d.getFullYear() ,d.getMonth()+1,
+               d.getDate()
+               ].join('-')+' '+
+              [d.getHours(),
+               d.getMinutes(),
+               d.getSeconds()].join(':');
+
+  var logException = {
+    err: params.error,
+    file_name: params.filename,
+    line_no: params.line,
+    col_no: params.column,
+    stack_trace: params.stackTrace,
+    referrer_url: req.headers.referer,
+    created_at: new Date(),
+    updated_at: new Date(),
+    client_time: dformat,
+    user_agent: params.userAgent,
+    all_params: JSON.stringify(params),
+    badge: params.badge
+  };
+  
+  var query = connection.query('INSERT INTO tbl_logs SET ?', logException, function(err, result) {
+    io.emit('exception-logged', {
+      requestedFrom: req.get('origin'),
+      data: req.body //obviously validate first
+    });
+    
+    res.send(JSON.stringify({
+      success: true
+    }));
+  });
+});
+
+http.listen(8080);
